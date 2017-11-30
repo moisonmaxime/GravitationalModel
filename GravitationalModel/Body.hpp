620fce27718dafb4e2eb04b58019563130371d9a
Maxime Moison <33101155+moisonmaxime@users.noreply.github.com>
2017-11-30 13:05:48 -0800

    comments

--- GravitationalModel/Body.hpp
+++ GravitationalModel/Body.hpp
@@ -12,23 +12,23 @@
 #include <list>
 #include "Vector3d.hpp"
 
-const double G = 6.67e-11;  # Gravitational Constant
+const double G = 6.67e-11;  // Gravitational Constant
 
 class Body {
 public:
-    string name;        # name of the body
-    double mass;        # mass of the body
-    double radius;      # radius of the body
-    Vector3d position;  # position of the body
-    Vector3d velocity;  # velocity of the body
+    string name;        // name of the body
+    double mass;        // mass of the body
+    double radius;      // radius of the body
+    Vector3d position;  // position of the body
+    Vector3d velocity;  // velocity of the body
     
-    Body(string name, double mass, double radius, Vector3d position, Vector3d velocity);    # Constructor
-    double distanceFrom(Body* other);                                                       # Distance form other body
-    double speed();                                                                         # Speed of body
-    Vector3d gravitationalForce(Body* other);                                               # Gravitational force from other
-    Vector3d acceleration(Body* other);                                                     # Acceleration from other body
-    Vector3d acceleration(list<Body*> others);                                              # Acceleration from multiple bodies
-    void apply(list<Body*> others, double seconds);                                         # Apply the forces to object
+    Body(string name, double mass, double radius, Vector3d position, Vector3d velocity);    // Constructor
+    double distanceFrom(Body* other);                                                       // Distance form other body
+    double speed();                                                                         // Speed of body
+    Vector3d gravitationalForce(Body* other);                                               // Gravitational force from other
+    Vector3d acceleration(Body* other);                                                     // Acceleration from other body
+    Vector3d acceleration(list<Body*> others);                                              // Acceleration from multiple bodies
+    void apply(list<Body*> others, double seconds);                                         // Apply the forces to object
 };
 
 #endif /* Body_hpp */
